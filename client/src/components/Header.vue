e2d80ad7c977f99d6a7d2d408033e3f5f0a604cd
WingedP <elysiawepts25@gmail.com>
2020-08-01 14:48:18 +0700

    fix client submodule

---
+++ client/src/components/Header.vue
@@ -0,0 +1,36 @@
+
+
+
+<template>
+    <header class="headerz">
+        <img :src="image" >
+    </header>
+</template>
+
+<script>
+export default {
+    name:"Header",
+    data(){return{
+        image: require('@/assets/apex.png')
+    }}
+  
+}
+</script>
+
+
+<style  scoped>
+img{
+height: 100%;
+}
+header{
+    /* background: blue; */
+    height: 11.5rem;
+}
+
+@media(max-width:700px){
+    img{
+    display: block;
+    margin: auto;
+}
+}
+</style>
